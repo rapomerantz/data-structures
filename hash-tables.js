b8c02b6bc2479cbcfebf15a5d154f392916cef1a
Atticus Pomerantz <atticus.pomerantz@dealerteamwork.com>
2020-08-25 15:19:08 -0500

    hash tables 1

---
+++ hash-tables.js
@@ -0,0 +1,66 @@
+/**
+ * aka dictionaries
+ * primary strength is super fast lookups
+ * example uses:
+ *      spellcheckers
+ *      dictionaries
+ *      compilers
+ *      code editors
+ *
+ * in Java -> HashMap
+ * in JS -> Object
+ * in Python/C# -> Dictionary
+ *
+ * Store key->value pairs
+ *
+ * 'hash function' sits between request & object, setting/returning address in the hash table
+ *      A hash function is DETERMINISTIC
+ *          this means that every time we give it the same input, it will return the same value
+ *
+ *  Hash table has 3 elements
+ *      Data
+ *          the values associated to the keys
+ *      Hash function
+ *          maps data of arbitrary size to a calculated index
+*       Storage buckets
+ *          the location that stores values to be accessed;
+ *
+ *
+ *  Operations: ALL IN CONSTANT TIME - O(1)
+ *      Insert
+ *
+ *      Lookup
+ *
+ *      Delete
+ *
+ *
+ *  containsKey: O(1)
+ *  ContainsValue : O(n)
+ *
+ **/
+
+//use a JS object to find the first non-repeated character in a string
+function findFirstNonRepeatedCharacter(string) {
+    const hashMap = {};
+    let result = null;
+    let stringToArray = string.split('');
+    stringToArray.forEach((character) => {
+        if (character in hashMap) {
+            hashMap[character] += 1;
+        } else {
+            hashMap[character] = 1;
+        }
+    });
+    const hashKeys = Object.keys(hashMap);
+    for (let i = 0; i < hashKeys.length; i ++) {
+        let key = hashKeys[i];
+        if (key !== ' ' && hashMap[key] === 1) {
+            result = key;
+            break;
+        }
+    }
+
+    return result;
+}
+
+console.log(findFirstNonRepeatedCharacter('taco meat'));
